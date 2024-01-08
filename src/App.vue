826d5651c31a63de9207ced799a23150315efc81
loaspra <santiago.madariaga@utec.edu.pe>
2024-01-08 00:07:39 -0500

    WIP

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,40 @@
   <v-app>
     <v-main>
       <HeroBanner/>
+      <v-container>
+          <CardGrid v-on:card-dragged="addCardToCanvas"/>
+          <MainCanvas :cards="selectedCards"/>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HeroBanner from './components/HeroBanner.vue'
+import CardGrid from './components/CardGrid.vue'
+import MainCanvas from './components/MainCanvas.vue'
 
 export default {
   name: 'App',
 
   components: {
     HeroBanner,
+    CardGrid,
+    MainCanvas
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      selectedCards: []
+    }
+  },
+
+  methods: {
+    addCardToCanvas(card) {
+      console.log('addCardToCanvas: CARD', card);
+      this.selectedCards.push(card);
+    }
+  }
 }
 </script>
 <style lang="scss" src="./style/style.scss"></style>
