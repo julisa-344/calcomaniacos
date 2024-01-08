08964a8a12ae086626d346ccd71c49362f1f26bc
loaspra <santiago.madariaga@utec.edu.pe>
2024-01-08 00:09:41 -0500

    Agregando componentes

---
+++ src/components/CardGrid.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card
+    v-for="(card, index) in cards"
+    :key="index"
+    draggable="true"
+    @dragStop="dragStart(card)"
+  >
+    <v-img :src="card.image" class="card-image"></v-img>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cards: [
+        { image: require('../assets/stickers/sticker1.png') },
+        { image: require('../assets/stickers/sticker2.jpg') },
+        // add more cards as needed
+      ],
+    };
+  },
+  methods: {
+    dragStart(card) {
+      this.$emit('cardDragged', card);
+    },
+  },
+  emits: ['cardDragged'],
+};
+</script>
+
+<style scoped>
+.card-image {
+  width: 250px;
+  height: 250px;
+}
+</style>
---
+++ src/components/MainCanvas.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    class="main-canvas"
+    @dragover.prevent
+    @drop="handleDrop"
+  >
+    <v-card
+      v-for="(card, index) in cards"
+      :key="index"
+    >
+      <v-img :src="card.image" class="card-image"></v-img>
+    </v-card>
+  </div>
+</template>
+w
+<script>
+export default {
+  props: {
+    cards: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    handleDrop(event) {
+      console.log(event);
+      const cardId = event.dataTransfer.getData('card-id');
+      // Emit an event to notify the parent component to add the card to the cards array
+      this.$emit('add-card', cardId);
+      console.log('wishuwishu', cardId);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.main-canvas {
+  width: 100%;
+  height: 100%;
+  background-color: #f5f5f5;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+</style>
