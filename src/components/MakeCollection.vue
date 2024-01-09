495a1570b1ad6a1c38c044807bf67e2ca162cfbe
Julisa Leon <julisa.leon344@gmail.com>
2024-01-09 10:46:06 -0500

    subiendo imagenes

---
+++ src/components/MakeCollection.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container class="flex bg-color m-width m-height p-4">
+        <MainCanvas :cards="selectedCards"/>
+        <CardGrid v-on:card-dragged="addCardToCanvas"/>
+    </v-container>
+</template>
+
+<script>
+import CardGrid from './CardGrid.vue';
+import MainCanvas from './MainCanvas.vue';
+export default {
+  components: { CardGrid, MainCanvas },
+    name: 'MakeCollection',
+    // Opciones del componente aquí
+}
+</script>
+
+<style scoped>
+/* Estilos del componente aquí */
+</style>
