d7130996a699f908deb40ac28e008a8e3e6a8742
Julisa Leon <julisa.leon344@gmail.com>
2024-01-10 17:59:03 -0500

    ssh

---
+++ src/components/shared/FilterSelect.vue
@@ -0,0 +1,23 @@
+<template>
+    <select v-model="selectedOption">
+        <option v-for="option in options" :value="option.value" :key="option.value">{{ option.label }}</option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'FilterSelect',
+    data() {
+        return {
+            selectedOption: null,
+            options: [
+                { label: 'ricky and morty', value: 'rick' },
+                { label: 'arctic monkey', value: 'arcticmonkey' },
+                { label: 'anime', value: 'anime' },
+                { label: 'messi', value: 'messi'},
+                { label: 'memes', value: 'memes'},
+            ]
+        };
+    }
+};
+</script>
